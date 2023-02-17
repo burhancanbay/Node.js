17c1c9006bfc410b76cb56c656b32a0e340b1bc7
burhancanbay <canbayburhan@gmail.com>
2023-02-17 17:47:46 +0300

    17/02/2023

--- RestAPI/models/Product.js
+++ RestAPI/models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  description: { type: String, required: true },
+  description: { type: String },
   createdAt: { type: Date, default: Date.now },
   user: { type: mongoose.Schema.Types.ObjectId },
 });
